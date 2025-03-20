261cccfb386937d63a71edf1398f44429a1a8258
Nas3ts <nkisowile@gmail.com>
2025-03-20 18:10:23 +0300

    feat(header): import header component in App.jsx

--- frontend/src/components/pages/App.jsx
+++ frontend/src/components/pages/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../../styles/App.css'
+import Header from '../layout/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
