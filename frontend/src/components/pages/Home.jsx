7a189f55a3f8fb784ac06f2d613bd29d7953fa08
Nas3ts <nkisowile@gmail.com>
2025-03-20 21:41:34 +0300

    feat(homepage): Add home page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,16 @@
 import NavigationBar from '../layout/NavigationBar.jsx'
+import styles from '../../styles/pages/Home.module.css'
 
 function Home() {
 
   return (
     <>
       <NavigationBar />
+
+      <div className={styles.intro}>
+        <h1>Your Perfect Laptop Awaits</h1>
+        <p>Compare Specs, Find Your Ideal Match.</p>
+      </div>
 
     </>
   )
