070646150e1fd6c3f3234253eb2e66a7dff8ae78
Nas3ts <nkisowile@gmail.com>
2025-03-22 09:16:26 +0300

    feat(homepage): Initialize home page layout

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import NavigationBar from '../layout/NavigationBar.jsx'
 import styles from '../../styles/pages/Home.module.css'
+import gearIcon from '../../assets/icons/icons8-gear-100.svg'
 
 function Home() {
 
@@ -8,8 +9,24 @@ function Home() {
       <NavigationBar />
 
       <div className={styles.intro}>
-        <h1>Your Perfect Laptop Awaits</h1>
-        <p>Compare Specs, Find Your Ideal Match.</p>
+        <div className={styles.introHero}>
+          <h1>Pick Purpose. Get Specs.<br/>See Available Devices.</h1>
+          <p>
+            We provide device specifications, 
+            pricing, and availability 
+            to help you find the right device
+            as per your needs and budget.
+          </p>
+          <button>
+            <img 
+              src={gearIcon} 
+              className={styles.gearIcon}
+            /> Get Started </button>
+        </div>
+        
+        <div className={styles.introImage}>
+        
+        </div>
       </div>
 
     </>
