2b4951a47eb2cabebd369a5a8fb0404d26087392
Geb.D <GebhardJr>
2025-04-15 18:36:26 +0300

    fix:fixed api inclusion

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -6,7 +6,7 @@ import Header from '../layout/Header.jsx'
 import Footer from '../layout/Footer.jsx'
 import { toast } from 'react-toastify'
 import { useState } from 'react'
-import api from '../utils/api'
+import api from '../../utils/api.js'
 
 // The Login component handles user login
 function Login() {
