621e5ab1cba3fe9478832bfbae35adf2ba8f7d4d
Geb.D <GebhardJr>
2025-04-15 20:23:36 +0300

    fix:switched react to React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import react from 'react'
-import { BrowserRouter , Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
 import Register from './components/pages/Register'
@@ -7,34 +7,34 @@ import NotFound from './components/pages/NotFound'
 // import ProtectedRoute from './components/features/ProtectedRoute'
 
 function Logout() {
-    localStorage.clear()
-    return <Navigate to="/login" />
-  }
-  
-  function RegisterAndLogout() {
-    localStorage.clear()
-    return <Register />
-  }
-  
-  function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/home"
-            element={
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path=""
+          element={
             //   <ProtectedRoute>
-                <Home />
+            <Home />
             //   </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<RegisterAndLogout />} />
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    )
-  }
-  
-  export default App
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
