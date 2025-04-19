e3c264bf3ff8ccadd61a655cdebc9b048b1ad01e
Nas3ts <nkisowile@gmail.com>
2025-04-19 10:12:55 +0300

    refactor: clean main and App components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,35 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import AppToastContainer from './components/features/Toastcontainer'
-import Register from './components/pages/customer/Register'
-import NotFound from './components/pages/NotFound'
-// import Profile from './components/pages/Profile'
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
-import Login from './components/pages/Login'
-import Home from './components/pages/Home'
-import React, { useEffect } from 'react'
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+// import AppToastContainer from './components/features/Toastcontainer'
+// import React, { useEffect } from 'react'
 
-function Logout() {
-  useEffect(() => {
-    localStorage.clear()
-  }, [])
-  return <Navigate to="/login" />
-}
+// function Logout() {
+//   useEffect(() => {
+//     localStorage.clear()
+//   }, [])
+//   return <Navigate to="/login" />
+// }
 
-function RegisterAndLogout() {
-  useEffect(() => {
-    localStorage.clear()
-  }, [])
-  return <Register />
-}
+// function RegisterAndLogout() {
+//   useEffect(() => {
+//     localStorage.clear()
+//   }, [])
+//   return <Register />
+// }
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header /> {/* This stays visible across all routes */}
-      <AppToastContainer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        {/* <Route path="/profile" element={<Profile />} /> */}
-        <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    
+    // <BrowserRouter>
+    //   <AppToastContainer />
+    //   <Routes>
+    //   </Routes>
+    // </BrowserRouter>
+
+    <div className="container mt-5">
+      <h1>Welcome to PCRS</h1>
+      <p>Let's recommend some laptops.</p>
+    </div>  
+    
   )
 }
 
