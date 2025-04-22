b44457299db1e84a20629c466cadba40bdc68117
Kristen05 <christopherchristine1@gmail.com>
2025-04-23 00:23:59 +0300

    created a product table

--- backend/login_and_register/models.py
+++ backend/login_and_register/models.py
@@ -23,38 +23,111 @@ class CustomUser(AbstractUser):
         return f"{self.first_name} {self.last_name}"
 
 
-class Vendor(models.Model):
-    user = models.OneToOneField(
-        CustomUser, on_delete=models.CASCADE, related_name="vendor_profile"
-    )
-    company_name = models.CharField(max_length=255)
-    location = models.CharField(
-        max_length=255
-    )  # can be extended with Google Maps API integration
-    verified = models.BooleanField(default=False)  # for admin to verify
+# class Vendor(models.Model):
+#     user = models.OneToOneField(
+#         CustomUser, on_delete=models.CASCADE, related_name="vendor_profile"
+#     )
+#     company_name = models.CharField(max_length=255)
+#     location = models.CharField(
+#         max_length=255
+#     )  # can be extended with Google Maps API integration
+#     verified = models.BooleanField(default=False)  # for admin to verify
 
-    def __str__(self):
-        return self.company_name
+#     def __str__(self):
+#         return self.company_name
 
+class Processor(models.Model):
+    brand= models.CharField(max_length=100)
+    model= models.CharField(max_length=100)
+    series= models.CharField(max_length=50)
+    generation= models.CharField(max_length=50)
+    core_count= models.IntegerField()
+    thread_count= models.IntegerField()
+    base_clock_speed= models.FloatField()
+    boost_clock_speed= models.FloatField()
+    cache_size= models.CharField(max_length=50)
+    integrated_graphics= models.BooleanField(max_length=50) 
 
-class Product(models.Model):
-    VENDOR_TYPES = [
-        ("laptop", "Laptop"),
-        ("desktop", "Desktop"),
-    ]
+    class RAM(models.Model): 
+        capacity_gb= models.IntegerField()
+        type= models.CharField(max_length=50)
+        frequency_mhz= models.IntegerField()
+        ecc_support= models.BooleanField()
+        channels= models.IntegerField()
 
-    vendor = models.ForeignKey(
-        Vendor, on_delete=models.CASCADE, related_name="products"
-    )
-    name = models.CharField(max_length=150)
-    type = models.CharField(max_length=50, choices=VENDOR_TYPES)
-    brand = models.CharField(max_length=100)
-    processor = models.CharField(max_length=100)
-    ram = models.CharField(max_length=50)
-    storage = models.CharField(max_length=50)
-    battery_life = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Storage(models.Model):
+        type= models.CharField(max_length=50)
+        form_factor= models.CharField(max_length=50)
+        capacity_gb= models.IntegerField()
+        interface= models.CharField(max_length=50)
+        rpm= models.IntegerField()
+        read_speed_mbps= models.IntegerField()
+        write_speed_mbps= models.IntegerField()
 
-    def __str__(self):
-        return f"{self.brand} {self.product_type} - {self.processor}"
+    class Graphic(models.Model):
+        gpu_type= models.CharField(max_length=50)
+        brand= models.CharField(max_length=50)
+        model= models.CharField(max_length=50)
+        vram_size_gb= models.IntegerField()
+        series= models.CharField(max_length=50)
+
+    class Display(models.Model):
+        size_inches= models.FloatField()
+        resolution= models.CharField(max_length=50)
+        panel_type= models.CharField(max_length=50)
+        refresh_rate_hz= models.IntegerField()
+        aspect_ratio= models.CharField(max_length=50)
+
+    class PortsConnectivity(models.Model):
+        usb_ports= models.IntegerField()
+        video_output= models.CharField()
+        ethernet_speed= models.FloatField()
+        wifi_version= models.CharField(max_length=50)
+        bluetooth_version= models.CharField(max_length=50)
+        audio_jack= models.BooleanField()
+        sd_card_reader= models.BooleanField()
+
+    class PowerBattery(models.Model):
+        battery_capacity_wh= models.FloatField()
+        adapter_wattage= models.IntegerField()
+        estimated_battery_life_hours= models.FloatField()
+
+    class Cooling(models.Model):
+        cooling_type= models.CharField(max_length=50)
+        fan_count= models.IntegerField()
+        cooler_brand= models.CharField(max_length=50)
+
+    class OperatingSystem(models.Model):
+        os_name= models.CharField(max_length=50)
+        version= models.CharField(max_length=50)
+        
+    class FormFactor(models.Model):
+        type= models.CharField(max_length=50)
+        dimensions= models.CharField(max_length=50)
+        weight= models.FloatField()
+
+    class Extra(models.Model):
+        keyboard_type= models.CharField(max_length=50)
+        has_backlit_keyboard= models.BooleanField()
+        webcam_resolution= models.CharField(max_length=50)
+        biometrics= models.CharField(max_length=50)
+        mil_std_certified= models.CharField(max_length=50)
+
+    class Product(models.Model):
+        processor= models.ForeignKey(Processor, on_delete=models.CASCADE)
+        ram= models.ForeignKey(RAM, on_delete=models.CASCADE)
+        storage= models.ForeignKey(Storage, on_delete=models.CASCADE)
+        graphic= models.ForeignKey(Graphic, on_delete=models.CASCADE)
+        display= models.ForeignKey(Display, on_delete=models.CASCADE)
+        ports_connectivity= models.ForeignKey(PortsConnectivity, on_delete=models.CASCADE)
+        power_battery= models.ForeignKey(PowerBattery, on_delete=models.CASCADE)
+        cooling= models.ForeignKey(Cooling, on_delete=models.CASCADE)
+        operating_system= models.ForeignKey(OperatingSystem, on_delete=models.CASCADE)
+        form_factor= models.ForeignKey(FormFactor, on_delete=models.CASCADE)
+        extra= models.ForeignKey(Extra, on_delete=models.CASCADE)
+       
+        
+    
+
+              
+        
