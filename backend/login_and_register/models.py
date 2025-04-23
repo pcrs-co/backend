8f452b9a0b0d3649f90a87b99dc622027887bc30
Kristen05 <christopherchristine1@gmail.com>
2025-04-23 13:30:58 +0300

    changes in alignment

--- backend/login_and_register/models.py
+++ backend/login_and_register/models.py
@@ -36,7 +36,7 @@ class CustomUser(AbstractUser):
 #     def __str__(self):
 #         return self.company_name
 
-    class Processor(models.Model):
+class Processor(models.Model):
         brand= models.CharField(max_length=100, blank=True, null=True)
         model= models.CharField(max_length=100, blank=True, null=True)
         series= models.CharField(max_length=50, blank=True, null=True)
@@ -48,14 +48,14 @@ class CustomUser(AbstractUser):
         cache_size= models.CharField(max_length=50, blank=True, null=True)
         integrated_graphics= models.BooleanField(max_length=50, blank=True, null=True) 
 
-    class RAM(models.Model): 
+class RAM(models.Model): 
         capacity_gb= models.IntegerField( blank=True, null=True)
         type= models.CharField(max_length=50, blank=True, null=True)
         frequency_mhz= models.IntegerField(blank=True, null=True)
         ecc_support= models.BooleanField( blank=True, null=True)
         channels= models.IntegerField( blank=True, null=True)
 
-    class Storage(models.Model):
+class Storage(models.Model):
         type= models.CharField(max_length=50, blank=True, null=True)
         form_factor= models.CharField(max_length=50, blank=True, null=True)
         capacity_gb= models.IntegerField( blank=True, null=True)
@@ -64,21 +64,21 @@ class CustomUser(AbstractUser):
         read_speed_mbps= models.IntegerField( blank=True, null=True)
         write_speed_mbps= models.IntegerField( blank=True, null=True)
 
-    class Graphic(models.Model):
+class Graphic(models.Model):
         gpu_type= models.CharField(max_length=50, blank=True, null=True)
         brand= models.CharField(max_length=50, blank=True, null=True)
         model= models.CharField(max_length=50, blank=True, null=True)
         vram_size_gb= models.IntegerField( blank=True, null=True)
         series= models.CharField(max_length=50, blank=True, null=True)
 
-    class Display(models.Model):
+class Display(models.Model):
         size_inches= models.FloatField( blank=True, null=True)
         resolution= models.CharField(max_length=50, blank=True, null=True)
         panel_type= models.CharField(max_length=50, blank=True, null=True)
         refresh_rate_hz= models.IntegerField( blank=True, null=True)
         aspect_ratio= models.CharField(max_length=50, blank=True, null=True)
 
-    class PortsConnectivity(models.Model):
+class PortsConnectivity(models.Model):
         usb_ports= models.IntegerField( blank=True, null=True)
         video_output= models.CharField( blank=True, null=True)
         ethernet_speed= models.FloatField( blank=True, null=True)
@@ -87,33 +87,33 @@ class CustomUser(AbstractUser):
         audio_jack= models.BooleanField( blank=True, null=True)
         sd_card_reader= models.BooleanField( blank=True, null=True)
 
-    class PowerBattery(models.Model):
+class PowerBattery(models.Model):
         battery_capacity_wh= models.FloatField( blank=True, null=True)
         adapter_wattage= models.IntegerField( blank=True, null=True)
         estimated_battery_life_hours= models.FloatField( blank=True, null=True)
 
-    class Cooling(models.Model):
+class Cooling(models.Model):
         cooling_type= models.CharField(max_length=50, blank=True, null=True)
         fan_count= models.IntegerField( blank=True, null=True)
         cooler_brand= models.CharField(max_length=50, blank=True, null=True)
 
-    class OperatingSystem(models.Model):
+class OperatingSystem(models.Model):
         os_name= models.CharField(max_length=50, blank=True, null=True)
         version= models.CharField(max_length=50, blank=True, null=True)
         
-    class FormFactor(models.Model):
+class FormFactor(models.Model):
         type= models.CharField(max_length=50, blank=True, null=True)
         dimensions= models.CharField(max_length=50, blank=True, null=True)
         weight= models.FloatField( blank=True, null=True)
 
-    class Extra(models.Model):
+class Extra(models.Model):
         keyboard_type= models.CharField(max_length=50, blank=True, null=True)
         has_backlit_keyboard= models.BooleanField( blank=True, null=True)
         webcam_resolution= models.CharField(max_length=50, blank=True, null=True)
         biometrics= models.CharField(max_length=50, blank=True, null=True)
         mil_std_certified= models.CharField(max_length=50, blank=True, null=True)
 
-    class Product(models.Model):
+class Product(models.Model):
         processor = models.ForeignKey('Processor', on_delete=models.CASCADE)
         ram= models.ForeignKey('RAM', on_delete=models.CASCADE)
         storage= models.ForeignKey('Storage', on_delete=models.CASCADE)
