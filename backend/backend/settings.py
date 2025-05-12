291f97e9d23cc5a04b0bbd9a231f6343246013a4
Geb.D <GebhardJr>
2025-05-12 08:42:48 +0300

    fix:changed gmail acc

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,8 +109,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "pcrs@fazen.tz"
-EMAIL_HOST_PASSWORD = "Pcrs@password3"
+EMAIL_HOST_USER = "contacts.pcrs@gmail.com"
+EMAIL_HOST_PASSWORD = "PCRS@student123"
 
 # Media configuration
 MEDIA_URL = "/media/"
