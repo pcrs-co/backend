c045736748a8500207e847d961ec6212ae846505
Geb.D <GebhardJr>
2025-05-14 23:08:08 +0300

    fix:updated the AdminSidebar

--- frontend/src/components/layout/admin/AdminSidebar.jsx
+++ frontend/src/components/layout/admin/AdminSidebar.jsx
@@ -6,6 +6,9 @@ import {
   People as CustomerIcon,
   Inventory as ProductIcon,
   Assignment as OrderIcon,
+  Apps as ApplicationIcon,
+  Memory as CpuIcon,
+  GraphicEq as GpuIcon,
   Logout as LogoutIcon
 } from '@mui/icons-material';
 
@@ -21,6 +24,9 @@ const AdminSidebar = () => {
           <MenuItemLink to="/admin/customers" primaryText="Customers" leftIcon={<CustomerIcon />} />
           <MenuItemLink to="/admin/products" primaryText="Products" leftIcon={<ProductIcon />} />
           <MenuItemLink to="/admin/orders" primaryText="Orders" leftIcon={<OrderIcon />} />
+          <MenuItemLink to="/admin/applications" primaryText="Applications" leftIcon={<ApplicationIcon />} />
+          <MenuItemLink to="/admin/cpu-benchmarks" primaryText="CPU Benchmarks" leftIcon={<CpuIcon />} />
+          <MenuItemLink to="/admin/gpu-benchmarks" primaryText="GPU Benchmarks" leftIcon={<GpuIcon />} />
           <MenuItemLink to="/logout" primaryText="Logout" leftIcon={<LogoutIcon />} />
         </List>
       </Box>
