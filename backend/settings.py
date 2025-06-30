34f7303af2101077d615466c8221a5fd43fc16df
fayepanga <fayepanga@gmail.com>
2025-06-30 16:27:51 +0300

    fix:for window users

--- backend/settings.py
+++ backend/settings.py
@@ -192,9 +192,11 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
+
 # CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 # ==============================================================================
 # CELERY SETTINGS
